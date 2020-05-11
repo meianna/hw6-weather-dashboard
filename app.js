9bac2d9e54c6f343614e3e59a2e7393f2dbcf662
Meianna Oeser <meianna.oeser@gmail.com>
2020-05-10 21:30:50 -0700

    added ajax request for uv index

--- app.js
+++ app.js
@@ -20,23 +20,34 @@ function currentWeather(cityName) {
     $("#icon").prepend(
       `<img src="https://openweathermap.org/img/wn/${res.weather[0].icon}.png">`
     );
-    $("#temp").text(`Temperature: ${res.main.temp}`);
+
+    // converting kelvin to fahrenheit for temperature
+    var kelvin = res.main.temp;
+    var fahrenheit = ((kelvin - 273.15) * 9) / 5 + 32;
+    var temp = fahrenheit.toFixed(0);
+    $("#temp").text(`Temperature: ${temp}°F`);
+
+    // not sure how to convert humidity value to %
     $("#humidity").text(`Humidity: ${res.main.humidity}`);
 
     // converting wind speed from meters per second to miles per hour
     var windSpeedMPS = res.wind.speed;
     var windSpeedMPH = windSpeedMPS * 2.237;
-    $("#wind").text(`Wind Speed: ${windSpeedMPH} mph`);
+    var wind = windSpeedMPH.toFixed(0);
+    $("#wind").text(`Wind Speed: ${wind} mph`);
 
-    // $("#currentWeather").append(
-    //   `<p>Current Weather: ${data.main.temp}</p>
-    //   <p>Wind: ${data.wind.speed}</p>
-    //   <p>Humidty: ${data.main.humidity}</p>
-    //   <p>Description: ${data.weather[0].main}</p>
-    //   <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">`
-    // );
+    // UV Index. This dats seems incorrect, way too high
     var lat = res.coord.lat;
     var lon = res.coord.lon;
+
+    $.ajax({
+      method: "GET",
+      url: `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
+      dataType: "json",
+    }).then(function (res) {
+      var uv = res.value.toFixed(0);
+      $("#uv").text(`UV Index: ${uv}`);
+    });
   });
 }
 
