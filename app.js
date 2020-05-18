24b72856642b82d65fac19d1fa819167e637a8ed
Meianna Oeser <meianna.oeser@gmail.com>
2020-05-17 17:46:58 -0700

    fixed local storage and search history

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
-  // declaring API key and current date in global scope
+  // declaring API key and current day/date in global scope
   var apiKey = "287b19e546f827a01d779e91f3882bc3";
+  var day = moment().format("dddd");
   var date = moment().format("l");
 
   // hiding currentWeather and futureWeather containers until submit button is clicked
   $("#currentWeather").hide();
   $("#futureWeather").hide();
 
-  // Submit button on click function triggers everything
+  // Submit button on click function
   $("#btnSubmit").on("click", function (e) {
     e.preventDefault();
     var cityName = $("#userInput").val();
     $("#userInput").val("");
     currentWeather(cityName);
     futureWeather(cityName);
+    searchHistory();
   });
 
   // current weather function
@@ -21,21 +23,24 @@ $(document).ready(function () {
     // showing currentWeather container
     $("#currentWeather").show();
 
-    // accessing weather API and console logging result
+    // accessing weather API
     $.ajax({
       method: "GET",
-      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`,
       dataType: "json",
     }).then(function (res) {
-      console.log(res);
-
       // adds search history to ul
-      $("#searchHistory").prepend(`<li class="render">${res.name}</li>`);
+      $("#searchHistory").prepend(`<li class="previousCity">${res.name}</li>`);
+      var previousSearch = JSON.parse(localStorage.getItem("city")) || [];
+      console.log(previousSearch);
+      previousSearch.push(res.name);
+      localStorage.setItem("city", JSON.stringify(previousSearch));
 
       // adds city name to html
       $("#cityName").text(res.name);
 
-      // adds current date to html
+      // adds current day and date to html
+      $("#day").text(day);
       $("#date").text(date);
 
       // adds icon of current weather to html
@@ -43,11 +48,8 @@ $(document).ready(function () {
         `<img src="https://openweathermap.org/img/wn/${res.weather[0].icon}.png">`
       );
 
-      // converting kelvin to fahrenheit for temperature and adding to html
-      var kelvin = res.main.temp;
-      var fahrenheit = ((kelvin - 273.15) * 9) / 5 + 32;
-      var temp = fahrenheit.toFixed(0);
-      $("#temp").text(`Temperature: ${temp}°F`);
+      // rounding temperature to whole number and adding to html
+      $("#temp").text(`Temperature: ${res.main.temp.toFixed(0)}°F`);
 
       // adds humidty to html
       $("#humidity").text(`Humidity: ${res.main.humidity}%`);
@@ -63,7 +65,7 @@ $(document).ready(function () {
       var lon = res.coord.lon;
 
       // creating separate API request for UV Index and adding to html.
-      // This dats seems incorrect, way too high. Can't figure out if I'm supposed to convert the value somehow
+      // This dats seems incorrect, way too high.
       $.ajax({
         method: "GET",
         url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
@@ -71,6 +73,13 @@ $(document).ready(function () {
       }).then(function (res) {
         var uv = res.value.toFixed(0);
         $("#uv").text(`UV Index: ${uv}`);
+        if ((uv) => 8) {
+          $("#uv").addClass("uvSevere");
+        } else if ((uv) => 5) {
+          $("#uv").addClass("uvModerate");
+        } else {
+          $("#uv").addClass("uvFavorable");
+        }
       });
     });
   }
@@ -83,68 +92,148 @@ $(document).ready(function () {
     // accessing forecast API and console logging result
     $.ajax({
       method: "GET",
-      url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`,
+      url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`,
       dataType: "json",
     }).then(function (res) {
-      console.log(res);
-
-      // adding forecast dates to card. I think there's a better way to do this...
-      $("#dateForecast1").text(res.list[2].dt_txt);
-      $("#dateForecast2").text(res.list[10].dt_txt);
-      $("#dateForecast3").text(res.list[18].dt_txt);
-      $("#dateForecast4").text(res.list[26].dt_txt);
-      $("#dateForecast5").text(res.list[34].dt_txt);
+      // adding forecast dates to card.
+      $("#dateForecast1").text(moment(res.list[0].dt_txt).format("l"));
+      $("#dateForecast2").text(moment(res.list[8].dt_txt).format("l"));
+      $("#dateForecast3").text(moment(res.list[16].dt_txt).format("l"));
+      $("#dateForecast4").text(moment(res.list[24].dt_txt).format("l"));
+      $("#dateForecast5").text(moment(res.list[32].dt_txt).format("l"));
 
       // adding forecast icons to card
       $("#iconForecast1").html(
-        `<img src="https://openweathermap.org/img/wn/${res.list[2].weather[0].icon}.png">`
+        `<img src="https://openweathermap.org/img/wn/${res.list[0].weather[0].icon}.png">`
       );
       $("#iconForecast2").html(
-        `<img src="https://openweathermap.org/img/wn/${res.list[10].weather[0].icon}.png">`
+        `<img src="https://openweathermap.org/img/wn/${res.list[8].weather[0].icon}.png">`
       );
       $("#iconForecast3").html(
-        `<img src="https://openweathermap.org/img/wn/${res.list[18].weather[0].icon}.png">`
+        `<img src="https://openweathermap.org/img/wn/${res.list[16].weather[0].icon}.png">`
       );
       $("#iconForecast4").html(
-        `<img src="https://openweathermap.org/img/wn/${res.list[26].weather[0].icon}.png">`
+        `<img src="https://openweathermap.org/img/wn/${res.list[24].weather[0].icon}.png">`
       );
       $("#iconForecast5").html(
-        `<img src="https://openweathermap.org/img/wn/${res.list[34].weather[0].icon}.png">`
+        `<img src="https://openweathermap.org/img/wn/${res.list[32].weather[0].icon}.png">`
       );
 
-      // converting the temp of each day of the forecast from kelvin to fahrenheit and adding to card
-      // feels like there should be an easier way to do this
-      var kelvinForecast1 = res.list[2].main.temp;
-      var fahrenheitForecast1 = ((kelvinForecast1 - 273.15) * 9) / 5 + 32;
-      var tempForecast1 = fahrenheitForecast1.toFixed(0);
-      $("#tempForecast1").text(`Temp: ${tempForecast1}°F`);
+      // adding forecast temp to cards
 
-      var kelvinForecast2 = res.list[10].main.temp;
-      var fahrenheitForecast2 = ((kelvinForecast2 - 273.15) * 9) / 5 + 32;
-      var tempForecast2 = fahrenheitForecast2.toFixed(0);
-      $("#tempForecast2").text(`Temp: ${tempForecast2}°F`);
-
-      var kelvinForecast3 = res.list[18].main.temp;
-      var fahrenheitForecast3 = ((kelvinForecast3 - 273.15) * 9) / 5 + 32;
-      var tempForecast3 = fahrenheitForecast3.toFixed(0);
-      $("#tempForecast3").text(`Temp: ${tempForecast3}°F`);
-
-      var kelvinForecast4 = res.list[26].main.temp;
-      var fahrenheitForecast4 = ((kelvinForecast4 - 273.15) * 9) / 5 + 32;
-      var tempForecast4 = fahrenheitForecast4.toFixed(0);
-      $("#tempForecast4").text(`Temp: ${tempForecast4}°F`);
-
-      var kelvinForecast5 = res.list[34].main.temp;
-      var fahrenheitForecast5 = ((kelvinForecast5 - 273.15) * 9) / 5 + 32;
-      var tempForecast5 = fahrenheitForecast5.toFixed(0);
-      $("#tempForecast5").text(`Temp: ${tempForecast5}°F`);
+      $("#tempForecast1").text(`Temp: ${res.list[0].main.temp.toFixed(0)}°F`);
+      $("#tempForecast2").text(`Temp: ${res.list[8].main.temp.toFixed(0)}°F`);
+      $("#tempForecast3").text(`Temp: ${res.list[16].main.temp.toFixed(0)}°F`);
+      $("#tempForecast4").text(`Temp: ${res.list[24].main.temp.toFixed(0)}°F`);
+      $("#tempForecast5").text(`Temp: ${res.list[32].main.temp.toFixed(0)}°F`);
 
       // adding humidity for each day to card
-      $("#humidityForecast1").text(`Humidity: ${res.list[2].main.humidity}%`);
-      $("#humidityForecast2").text(`Humidity: ${res.list[10].main.humidity}%`);
-      $("#humidityForecast3").text(`Humidity: ${res.list[18].main.humidity}%`);
-      $("#humidityForecast4").text(`Humidity: ${res.list[26].main.humidity}%`);
-      $("#humidityForecast5").text(`Humidity: ${res.list[34].main.humidity}%`);
+      $("#humidityForecast1").text(`Humidity: ${res.list[0].main.humidity}%`);
+      $("#humidityForecast2").text(`Humidity: ${res.list[8].main.humidity}%`);
+      $("#humidityForecast3").text(`Humidity: ${res.list[16].main.humidity}%`);
+      $("#humidityForecast4").text(`Humidity: ${res.list[24].main.humidity}%`);
+      $("#humidityForecast5").text(`Humidity: ${res.list[32].main.humidity}%`);
+    });
+  }
+
+  // search history on click function
+  function searchHistory() {
+    var searchHistory = JSON.parse(localStorage.getItem("city")) || [];
+    $("#searchHistory").empty();
+    for (i = 0; i < searchHistory.length; i++) {
+      $("#searchHistory").prepend(
+        `<li class="previousCity">${searchHistory[i]}</li>`
+      );
+    }
+
+    $(document).on("click", ".previousCity", function () {
+      var text = $(this).text();
+      console.log(text);
+
+      $.ajax({
+        method: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${apiKey}&units=imperial`,
+        dataType: "json",
+      }).then(function (res) {
+        $("#cityName").text(res.name);
+        $("#day").text(day);
+        $("#date").text(date);
+        $("#icon").html(
+          `<img src="https://openweathermap.org/img/wn/${res.weather[0].icon}.png">`
+        );
+        $("#temp").text(`Temperature: ${res.main.temp.toFixed(0)}°F`);
+        $("#humidity").text(`Humidity: ${res.main.humidity}%`);
+        var windSpeedMPS = res.wind.speed;
+        var windSpeedMPH = windSpeedMPS * 2.237;
+        var wind = windSpeedMPH.toFixed(0);
+        $("#wind").text(`Wind Speed: ${wind} mph`);
+        var lat = res.coord.lat;
+        var lon = res.coord.lon;
+        $.ajax({
+          method: "GET",
+          url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
+          dataType: "json",
+        }).then(function (res) {
+          var uv = res.value.toFixed(0);
+          $("#uv").text(`UV Index: ${uv}`);
+          if ((uv) => 8) {
+            $("#uv").addClass("uvSevere");
+          } else if ((uv) => 5) {
+            $("#uv").addClass("uvModerate");
+          } else {
+            $("#uv").addClass("uvFavorable");
+          }
+        });
+      });
+
+      $.ajax({
+        method: "GET",
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${text}&appid=${apiKey}&units=imperial`,
+        dataType: "json",
+      }).then(function (res) {
+        $("#dateForecast1").text(moment(res.list[0].dt_txt).format("l"));
+        $("#dateForecast2").text(moment(res.list[8].dt_txt).format("l"));
+        $("#dateForecast3").text(moment(res.list[16].dt_txt).format("l"));
+        $("#dateForecast4").text(moment(res.list[24].dt_txt).format("l"));
+        $("#dateForecast5").text(moment(res.list[32].dt_txt).format("l"));
+        $("#iconForecast1").html(
+          `<img src="https://openweathermap.org/img/wn/${res.list[0].weather[0].icon}.png">`
+        );
+        $("#iconForecast2").html(
+          `<img src="https://openweathermap.org/img/wn/${res.list[8].weather[0].icon}.png">`
+        );
+        $("#iconForecast3").html(
+          `<img src="https://openweathermap.org/img/wn/${res.list[16].weather[0].icon}.png">`
+        );
+        $("#iconForecast4").html(
+          `<img src="https://openweathermap.org/img/wn/${res.list[24].weather[0].icon}.png">`
+        );
+        $("#iconForecast5").html(
+          `<img src="https://openweathermap.org/img/wn/${res.list[32].weather[0].icon}.png">`
+        );
+        $("#tempForecast1").text(`Temp: ${res.list[0].main.temp.toFixed(0)}°F`);
+        $("#tempForecast2").text(`Temp: ${res.list[8].main.temp.toFixed(0)}°F`);
+        $("#tempForecast3").text(
+          `Temp: ${res.list[16].main.temp.toFixed(0)}°F`
+        );
+        $("#tempForecast4").text(
+          `Temp: ${res.list[24].main.temp.toFixed(0)}°F`
+        );
+        $("#tempForecast5").text(
+          `Temp: ${res.list[32].main.temp.toFixed(0)}°F`
+        );
+        $("#humidityForecast1").text(`Humidity: ${res.list[0].main.humidity}%`);
+        $("#humidityForecast2").text(`Humidity: ${res.list[8].main.humidity}%`);
+        $("#humidityForecast3").text(
+          `Humidity: ${res.list[16].main.humidity}%`
+        );
+        $("#humidityForecast4").text(
+          `Humidity: ${res.list[24].main.humidity}%`
+        );
+        $("#humidityForecast5").text(
+          `Humidity: ${res.list[32].main.humidity}%`
+        );
+      });
     });
   }
 });
